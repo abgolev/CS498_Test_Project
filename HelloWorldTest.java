a76763b4bb30aa743725d3a92d9d8951b46e2183
Stephen Ritchie <stephenfritchie@gmail.com>
2018-04-11 10:58:09 -0400

    Initial Commit

---
+++ HelloWorldTest.java
@@ -0,0 +1,57 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloWorldTest {
+
+	@Test
+	void test1() {
+		assertEquals("foo", "foo");
+	}
+	
+	@Test
+	void test2() {
+		assertEquals("foo", "foo");
+	}
+	
+	@Test
+	void test3() {
+		assertEquals("foo", "foo");
+	}
+	
+	@Test
+	void test4() {
+		assertEquals("foo", "foo");
+	}
+	
+	@Test
+	void test5() {
+		assertEquals("foo", "foo");
+	}
+	
+	@Test
+	void test6() {
+		assertEquals("foo", "foo");
+	}
+	
+	@Test
+	void test7() {
+		assertEquals("foo", "foo");
+	}
+	
+	@Test
+	void test8() {
+		assertEquals("foo", "foo");
+	}
+	
+	@Test
+	void test9() {
+		assertEquals("foo", "foo");
+	}
+	
+	@Test
+	void test10() {
+		assertEquals("foo", "foo");
+	}
+
+}
